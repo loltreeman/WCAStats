a8b6a5f598e55b58d3607432099fcc134563f072
loltreeman <110177022+loltreeman@users.noreply.github.com>
2024-09-04 21:11:11 +0800

    Create average_average_finalists.sql

---
+++ average_average_finalists.sql
@@ -0,0 +1,22 @@
+-- Average average of all 3x3 finalists per year in competitors in Philippines, in comps where 3x3 was held more than 1 round
+
+SELECT year, competitionId, AVG(avg_of_mean) AS avg_avg_mean
+FROM (
+    SELECT YEAR(c.end_date) AS year, r.competitionId, AVG(r.average) / 100 AS avg_of_mean
+    FROM Results r
+    JOIN Competitions c ON r.competitionId = c.id
+    WHERE r.eventId = "333"
+      AND r.roundTypeId IN ("c", "f")
+      AND r.average > 0
+      AND c.countryId = "Philippines"  
+      AND r.competitionId IN (
+          SELECT competitionId
+          FROM Results
+          WHERE eventId = "333"
+          GROUP BY competitionId
+          HAVING COUNT(DISTINCT roundTypeId) > 1
+      )
+    GROUP BY r.competitionId, YEAR(c.end_date)
+) AS subquery
+GROUP BY year, competitionId
+ORDER BY year, competitionId;
