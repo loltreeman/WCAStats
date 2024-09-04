59315c6ef6c0e3417f4f1139e6abcc632823f696
loltreeman <110177022+loltreeman@users.noreply.github.com>
2024-09-04 20:18:36 +0800

    Create best_to_worst_singles_any_events.sql

---
+++ best_to_worst_singles_any_events.sql
@@ -0,0 +1,17 @@
+# Rankings of Best To worst Single for any event
+
+SELECT personId, competitionId, roundTypeId, 1 as solve, value1 as value FROM Results r
+WHERE r.countryId = ':country_id' AND r.eventId = ':event_id' AND value1 > 0
+UNION ALL
+SELECT personId, competitionId, roundTypeId, 2, value2 as value FROM Results r
+WHERE r.countryId = ':country_id' AND r.eventId = ':event_id' AND value2 > 0
+UNION ALL
+SELECT personId, competitionId, roundTypeId, 3, value3 as value FROM Results r
+WHERE r.countryId = ':country_id' AND r.eventId = ':event_id' AND value3 > 0
+UNION ALL
+SELECT personId, competitionId, roundTypeId, 4, value4 as value FROM Results r
+WHERE r.countryId = ':country_id' AND r.eventId = ':event_id' AND value4 > 0
+UNION ALL
+SELECT personId, competitionId, roundTypeId, 5, value5 as value FROM Results r
+WHERE r.countryId = ':country_id' AND r.eventId = ':event_id' AND value5 > 0
+ORDER BY value
